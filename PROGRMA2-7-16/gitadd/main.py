f276b524ad44ef0e23d16bdb4dffa817483697b2
Luis Manuel Alaya Cabrera <Luis Manuel Alaya Cabrera>
2016-07-03 01:18:23 -0500

    1:18am

--- PROGRMA2-7-16/gitadd/main.py
+++ PROGRMA2-7-16/gitadd/main.py
@@ -94,24 +94,69 @@ class ThirdWindow(QtGui.QMainWindow, platosalacarta.Ui_pcWindow):
         self.setupUi(self)
         self.btnIzquierda4.clicked.connect(self.btnIzquierda4Clicked)
         self.btnPlatosalacarta0.clicked.connect(self.btnPlatosalacarta0Clicked)
+        self.btnLomosaltado0.clicked.connect(self.btnLomosaltado0Clicked)
+        self.btnPollosaltado0.clicked.connect(self.btnPollosaltado0Clicked)
+        self.btnAlfredo0.clicked.connect(self.btnAlfredo0Clicked)
+        self.btnTallarin0.clicked.connect(self.btnTallarin0Clicked)
+        self.btnChifa0.clicked.connect(self.btnChifa0Clicked)
+        self.btnCubana0.clicked.connect(self.btnCubana0Clicked)
+        self.tenW = None
+        self.elevenW = None
+        self.twelveW = None
+        self.thirteenW = None
+        self.fourteenW = None
+        self.fifteenW = None
 
     def btnIzquierda4Clicked(self):
         self.close()
 
-
     def btnPlatosalacarta0Clicked(self):
         self.close()
 
+    def btnLomosaltado0Clicked(self):
+        if self.tenW is None:
+            self.tenW = TenWindow(self)
+        self.tenW.show()
 
+    def btnPollosaltado0Clicked(self):
+        if self.elevenW is None:
+            self.elevenW = ElevenWindow(self)
+        self.elevenW.show()
 
+    def btnAlfredo0Clicked(self):
+        if self.twelveW is None:
+            self.twelveW = TwelveWindow(self)
+        self.twelveW.show()
 
+    def btnTallarin0Clicked(self):
+        if self.thirteenW is None:
+            self.thirteenW = ThirteenWindow(self)
+        self.thirteenW.show()
 
+    def btnChifa0Clicked(self):
+        if self.fourteenW is None:
+            self.fourteenW = FourteenWindow(self)
+        self.fourteenW.show()
+
+    def btnCubana0Clicked(self):
+        if self.fifteenW is None:
+            self.fifteenW = FifteenWindow(self)
+        self.fifteenW.show()
 
 
 class FourWindow(QtGui.QMainWindow, panes.Ui_panesWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setupUi(self)
+
+
+
+
+
+
+
+
+
 
 class SevenWindow(QtGui.QMainWindow, tercerapantalla.Ui_terceraWindow):
     def __init__(self, parent=None):
@@ -127,6 +172,44 @@ class FiveWindow(QtGui.QMainWindow, tercerapantalla2.Ui_tercera2Window):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setupUi(self)
+
+class TenWindow(QtGui.QMainWindow, platosalacarta1.Ui_pc1Window):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+class ElevenWindow(QtGui.QMainWindow, platosalacarta2.Ui_pc2Window):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+class TwelveWindow(QtGui.QMainWindow, platosalacarta3.Ui_pc3Window):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+class ThirteenWindow(QtGui.QMainWindow, platosalacarta4.Ui_pc4Window):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+class FourteenWindow(QtGui.QMainWindow, platosalacarta5.Ui_pc5Window):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+class FifteenWindow(QtGui.QMainWindow, platosalacarta6.Ui_pc6Window):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+
+
+
+
+
+
+
 
 def main():
     app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
